3e08ce8ef47f893abdbda48a3c377ccc28cb0cdf
Chris <helm.chris82@gmail.com>
2020-04-12 19:22:52 -0400

    change all to double quotes in signup index.js

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import './signup.css';
-import avatar from './avatar.png';
+import React from "react";
+import "./signup.css";
+import avatar from "./avatar.png";
 
 function signup() {
   return (
-    <div className='signupbox'>
-      <img src={avatar} className='avatar'/>
+    <div className="signupbox">
+      <img src={avatar} className="avatar" />
       <h1>Sign Up</h1>
       <form>
         <p>Email</p>
-        <input type='text' name='' placeholder='Enter Email' />
+        <input type="text" name="" placeholder="Enter Email" />
         <p>Name</p>
-        <input type='Name' name='' placeholder='Name' />
+        <input type="Name" name="" placeholder="Name" />
         <p>Password</p>
-        <input type='Password' name='' placeholder='Enter Password'/>
-        <input type='submit' name='' value='Sign Up'/>
+        <input type="Password" name="" placeholder="Enter Password" />
+        <input type="submit" name="" value="Sign Up" />
         <br />
-        <a href='#'>Lost your password?</a>
+        <a href="#">Lost your password?</a>
         <br />
-        <a href='#'>Don't have an account?</a>
+        <a href="#">Don't have an account?</a>
       </form>
     </div>
   );
