50d5c822f14cac4cfd41b0ed90b4d2f70786b150
Ryan <larsenr12@gmail.com>
2020-04-25 12:44:18 -0400

    Updated master

--- routes/api/meals.js
+++ routes/api/meals.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const router = express.router
+// const express = require('express')
+// const router = express.router
 
-// Meals model
-const Meal = require('../../models/Meal')
+// // Meals model
+// const Meal = require('../../models/Meal')
 
-// Get all meals
-router.get('/', (req, res) => {
-  Meal.find()
-    .sort({ date: -1 })
-    .then(meals => res.json(meals))
-})
+// // Get all meals
+// router.get('/', (req, res) => {
+//   Meal.find()
+//     .sort({ date: -1 })
+//     .then(meals => res.json(meals))
+// })
 
-module.export = router
+// module.export = router
