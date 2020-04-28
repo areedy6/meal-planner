aeb0a94c9da26ac28ad59ba0490b547951067ddc
Ryan <larsenr12@gmail.com>
2020-04-28 16:25:32 -0400

    Updated app.css

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -24,6 +24,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../App.css'
+import '../pages/Login/login'
+import '../pages/signup/signup'
+import '../App'
 
 function Nav () {
   return (
